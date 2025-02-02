cb9ea0bc76f76b692f77b4e637d0c991ee0a688b
zatway <radmirar00@mail.ru>
2025-02-02 20:01:52 +0500

    1

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {TextField} from "@mui/material";
 
 const MainPage = () => {
     return (
         <div>
-            
+            <TextField></TextField>
         </div>
     );
 };
