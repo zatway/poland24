f3e233928ca85adf6db5bda82a9c6951aa1adb23
zatway <radmirar00@mail.ru>
2025-02-02 16:58:17 +0500

    config create

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env) => {
+    const isProduction = env.mode === 'production';
+
+    return {
+        mode: isProduction ? 'production' : 'development',
+        entry: './src/index.tsx',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js'],
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+            ],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html',
+            }),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'public'),
+            },
+            compress: true,
+            port: 3000,
+            open: true,
+        },
+    };
+};
